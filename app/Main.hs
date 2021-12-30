80f51eb656b1ec6e376180573faab6527cc074f1
Quim Casals <quim.casals@gmail.com>
2021-12-30 10:28:21 +0100

    Something kind of works

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
 import Control.Applicative
 import qualified Data.ByteString.Lazy as BL
-import Data.Csv
-import qualified Data.Vector as V
-import Text.RawString.QQ
-import Text.Regex.TDFA
 import Data.Time
+import System.Environment
+import Options.Applicative
+import Options.Applicative.Help.Pretty
+import Text.Read (readMaybe)
 import Data.List
--- import Data.Vector
+import Lib
 
 
 test :: Maybe [PrePeriod] -> Maybe [Period]
@@ -21,30 +22,72 @@ test l = case l of
 test2 :: IO (Maybe [PrePeriod]) -> IO (Maybe [Period])
 test2 l = test <$> l
 
-test3 :: Maybe [Period] -> Maybe [Period]
-test3 l = case l of
-            Nothing -> Nothing
-            Just l' -> return $ sort l'
+-- test3 :: Maybe [Period] -> Maybe [Period]
+-- test3 l = case l of
+--             Nothing -> Nothing
+--             Just l' -> return $ sort l'
 
-aux :: Maybe [PrePeriod] -> Maybe [Period]
-aux l = fmap sort $ l >>= mapM fromPreToPeriod
+-- preProcess :: Maybe [PrePeriod] -> Maybe [Period]
+-- preProcess l = fmap sort $ l >>= mapM fromPreToPeriod
 
+-- what = map (map periodToWorkingDays) <$> days
 initDate = fromGregorian 2020 10 01
 todayDate = fromGregorian 2021 12 27
 s = fromGregorian 2021 12 08
 s2 = fromGregorian 2021 12 11
 e = fromGregorian 2021 12 16
 
-main :: IO ()
-main = do
-  list <- readfile
-  case list of
-    Nothing -> putStrLn "nothing to show"
-    Just l -> mapM_ (print . fromPreToPeriod) l
+-- days = aux <$> readfile
+-- od0 = OffDaysInfo {lastUpdate = initDate, availableDays = 0, usedDays = 0}
+-- updateInfo = (days, od0)
+
+-- subseq = fmap inits <$> days
+-- mytrace = fmap (map (foldl updateOffDays od0))  <$> subseq
+
 -- main :: IO ()
 -- main = do
---     csvData <- BL.readFile "data/calendar1.csv"
---     case decodeByName csvData of
---         Left err -> putStrLn err
---         Right (_, v) -> V.forM_ v $ \ p ->
---             putStrLn $ start p ++ end p ++ non_working p ++ total p
+--   dates <- readfile
+--   case list of
+--     Nothing -> putStrLn "nothing to show"
+--     Just l -> print $ fmap (map (foldl updateOffDays od0)) list
+
+data Options = Options {
+  _initDate :: String,
+  _datesFile :: FilePath
+  }
+
+parseOptions :: Parser Options
+parseOptions = Options
+               <$> strOption
+               (long "initial-date"
+                <> short 'd'
+                <> metavar "init_date"
+                <> showDefault
+                <> value "build"
+                <> help "Date to start calculating offdays")
+               <*> strOption
+               (metavar "DATES_FILE"
+                <> short 'f'
+                <> help "Path to a csv with the offdays dates")
+
+descr :: ParserInfo Options
+descr = info (parseOptions <**> helper)
+       (fullDesc
+        <> progDesc "Parse an org mode file with a datalang specification and compile it to all available formats."
+        <> headerDoc (Just $ dullblue $ bold $ underline "datalang help")
+        <> footerDoc (Just foot)
+       )
+  where
+  foot :: Doc
+  foot = bold "maintainer: " <> "Quim"
+
+
+go :: Options -> IO ()
+go Options{..} = do
+  dates <- readfile _datesFile
+  case dates of
+    Nothing -> print "Something went wrong"
+    Just d -> print $ preProcessPeriods d >>= computeOffDaySeq _initDate
+
+main :: IO ()
+main = execParser descr >>= go
--- src/Lib.hs
+++ src/Lib.hs
@@ -15,21 +15,20 @@ import Data.Csv
 import qualified Data.Vector as V
 import Data.Time
 import Data.List.Split
+import Data.List
 import Text.Read (readMaybe)
 
 data PrePeriod = PrePeriod
     { start :: !String
     , end:: !String
-    , non_working:: !String
-    , total:: !String
     } deriving (Show)
 
 instance FromNamedRecord PrePeriod where
-    parseNamedRecord r = PrePeriod <$> r .: "Start" <*> r .: "End" <*> r .: "Non-working" <*> r .: "Total"
+    parseNamedRecord r = PrePeriod <$> r .: "Start" <*> r .: "End"
 
 data Period = Period
     { pstart :: Day
-    , pend:: Day
+    , pend :: Day
     } deriving (Show, Eq)
 instance Ord Period where
   (Period s1 _) `compare` (Period s2 _) = s1 `compare` s2
@@ -58,31 +57,72 @@ fromPreToPeriod p = do
   where (yearS, monthS, dayS) = splitDate (start p)
         (yearE, monthE, dayE) = splitDate (end p)
 
+getPeriods :: [PrePeriod] -> Maybe [Period]
+getPeriods l = sort <$> mapM fromPreToPeriod l
+
 countWeekends :: Day -> Day -> Integer
 countWeekends s e = toInteger $ length (filter isweekend list)
   where list = takeWhile (<= e) (iterate (addDays 1) s)
         isweekend d = dayOfWeek d == Saturday || dayOfWeek d == Sunday
+
+computeWorkingDays :: Period -> Integer
+computeWorkingDays p = howManyWorking (pstart p) (pend p)
 
 howManyWorking :: Day -> Day -> Integer
 howManyWorking s e = total - weekend
   where  total = diffDays e s + 1
          weekend = countWeekends s e
 
+spentDays :: [Period] -> Integer
+spentDays = sum . map computeWorkingDays
+
 daysoffPerMonth :: Float
 daysoffPerMonth = 2.5
 
-generatedOffDays :: Day -> Day -> Integer -> Integer
-generatedOffDays s e usedDays = if generatedDays - usedDays > flooredDays then flooredDays
-                                else generatedDays - usedDays
+remainingOffDays :: Day -> Day -> Integer -> Integer
+remainingOffDays s e usedDays = min (generatedDays - usedDays) flooredDays
   where currentMonths = diffGregorianDurationClip e s
         generatedDays = floor $ fromInteger (cdMonths currentMonths) * daysoffPerMonth
         flooredDays = floor $ 12 * daysoffPerMonth
 
-readfile :: IO (Maybe [PrePeriod])
-readfile = do
-  csvData <- BL.readFile "data/calendar1.csv"
+computeOffDays :: Day -> Period -> Integer
+computeOffDays i0 p = min generatedDays flooredDays
+  where s = pstart p
+        e = pend p
+        currentMonths = diffGregorianDurationClip s i0
+        generatedDays = floor $ fromInteger (cdMonths currentMonths) * daysoffPerMonth
+        flooredDays = floor $ 12 * daysoffPerMonth
+
+data OffDaysInfo = OffDaysInfo { lastUpdate :: Day,
+                                 availableDays :: Integer,
+                                 usedDays :: Integer } deriving (Show)
+
+updateOffDays :: OffDaysInfo -> Period -> OffDaysInfo
+updateOffDays i p = OffDaysInfo { lastUpdate = addDays (negate (cdDays currentMonths)) (pend p),
+                                  availableDays = min newAvailableDays flooredDays - newUsedDays,
+                                  usedDays = newUsedDays }
+  where newUsedDays = computeWorkingDays p
+        currentMonths = diffGregorianDurationClip (pend p) (lastUpdate i)
+        newAvailableDays = computeOffDays (lastUpdate i) p + availableDays i
+        flooredDays = floor $ 12 * daysoffPerMonth
+
+readfile :: String -> IO (Maybe [PrePeriod])
+readfile f = do
+  csvData <- BL.readFile f
   case helper csvData of
     Left err -> return Nothing
     Right v -> return $ Just $ V.toList v
     where helper :: BL.ByteString -> Either String (V.Vector PrePeriod)
           helper = fmap snd . decodeByName
+
+preProcessPeriods :: [PrePeriod] -> Maybe [Period]
+preProcessPeriods = fmap sort.mapM fromPreToPeriod
+
+computeOffDaySeq :: String -> [Period] -> Maybe OffDaysInfo
+computeOffDaySeq s l = do
+        yearS <- readMaybe year :: Maybe Integer
+        monthS <- readMaybe month :: Maybe Int
+        dayS <- readMaybe day :: Maybe Int
+        date0 <- fromGregorianValid yearS monthS dayS
+        return $ foldl updateOffDays OffDaysInfo {lastUpdate = date0, availableDays = 0, usedDays = 0} l
+  where (year, month, day) = splitDate s
